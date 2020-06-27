65b7c2ff0443f541b70f2d637a2d54b7737eb5ab
chenzy01 <chenzy.hyx@gmail.com>
2020-06-27 23:54:01 +0800

    first commit

---
+++ debugtalk.py
@@ -0,0 +1,13 @@
+
+import time
+
+
+def sleep(n_secs):
+    time.sleep(n_secs)
+
+
+def get_accounts():
+    return [
+        {'phone': '15820723515', 'password':'101402108mynba'},
+        {'phone': '158207235', 'password': '101402108myn21'},
+    ]
